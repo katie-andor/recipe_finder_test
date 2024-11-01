90bc560afdc4c88ed729dfb16bc627c7b06dfd0c
Katie Andor <superkga5678@gmail.com>
2024-11-01 15:52:21 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes instead of Switch
 import './index.css';
-import App from './App';
+import RecipeSearch from './RecipeSearch';
+import RecipeDetails from './RecipeDetails'; // Import the new RecipeDetails component
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<RecipeSearch />} /> {/* Use element prop instead of component */}
+        <Route path="/recipes/:id" element={<RecipeDetails />} /> {/* Route for recipe details */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
